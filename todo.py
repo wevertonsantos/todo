35a9fd831ce73d8a1939d04c30da9738be1673e2
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-03 19:14:22 -0300

    Exemplo de todo

---
+++ todo.py
@@ -0,0 +1,11 @@
+'''
+Adicionar tarefa: você escreve o que precisa ser feito. Exemplo: "Estudar inglês".
+
+Listar tarefas: todas aparecem na lista, geralmente com uma checkbox (caixinha) ao lado.
+
+Marcar como feita: quando terminar, marca a caixinha ou clica em “concluído”.
+
+Remover tarefa (opcional): a tarefa pode ser apagada ou arquivada.
+
+Editar ou reordenar tarefas (opcional): você pode mudar o texto ou a ordem de prioridade.
+'''
