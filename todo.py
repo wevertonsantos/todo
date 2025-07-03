8a99ee257f3cdceb0233b976fdc9cad8f920cb25
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-03 19:31:49 -0300

    fazendo loop while para não fechar as opções assim que sai do programa e adicionando uma nova escolha de número 5

--- todo.py
+++ todo.py
@@ -12,13 +12,17 @@ Editar ou reordenar tarefas (opcional): você pode mudar o texto ou a ordem de p
 
 # criando lista
 lista_tarefas = []
-# pedindo para o usuário que escolha alguma das opções
-escolha = int(input("Opções: 1 - Mostrar lista, 2 - Adicionar tarefa. O que deseja da sua To-Do List? "))
-
-if escolha == 1:
-    for tarefa in lista_tarefas:
-        print("Tarefas:")
-        print(f"{lista_tarefas.index(tarefa) + 1} - {tarefa}") # mostrando cada tarefa
-elif escolha == 2:
-    nova_tarefa = input("Qual tarefa deseja colocar na lista?") # perguntando qual tarefa deseja incluir
-    lista_tarefas.append(nova_tarefa) # adicionando tarefa a lista de tarefas
+while True:
+    # pedindo para o usuário que escolha alguma das opções
+    escolha = int(input("Opções: 1 - Mostrar lista, 2 - Adicionar tarefa. O que deseja da sua To-Do List? "))
+    if escolha == 1:
+        for tarefa in lista_tarefas:
+            print("Tarefas:")
+            print(f"{lista_tarefas.index(tarefa) + 1} - {tarefa}") # mostrando cada tarefa
+    elif escolha == 2:
+        nova_tarefa = input("Qual tarefa deseja colocar na lista? ") # perguntando qual tarefa deseja incluir
+        lista_tarefas.append(nova_tarefa) # adicionando tarefa a lista de tarefas
+        print("Tarefa incluída com sucesso")
+    elif escolha == 5:
+        print("Volte sempre! :)")
+        break
