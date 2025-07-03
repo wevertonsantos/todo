3070bd0e1c557183c551f9db4dff7458a58c1c43
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-03 19:21:21 -0300

    mostrando lista de tarefas.

--- todo.py
+++ todo.py
@@ -14,3 +14,6 @@ Editar ou reordenar tarefas (opcional): você pode mudar o texto ou a ordem de p
 lista_tarefas = []
 # pedindo para o usuário que escolha alguma das opções
 escolha = input("O que deseja fazer na sua To-Do List? Opções: 1 - Mostrar lista, 2 - Adicionar tarefa")
+
+if escolha == 1:
+    print(lista_tarefas) # mostrando a lista de tarefas
