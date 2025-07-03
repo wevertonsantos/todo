47048c4ca2decf892aac63784ae842baee8b3770
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-03 19:27:24 -0300

    fazendo for em lista de tarefas e mostrando cada tarefa.

--- todo.py
+++ todo.py
@@ -11,12 +11,14 @@ Editar ou reordenar tarefas (opcional): você pode mudar o texto ou a ordem de p
 '''
 
 # criando lista
-lista_tarefas = []
+lista_tarefas = [0]
 # pedindo para o usuário que escolha alguma das opções
 escolha = int(input("Opções: 1 - Mostrar lista, 2 - Adicionar tarefa. O que deseja da sua To-Do List? "))
 
 if escolha == 1:
-    print(lista_tarefas) # mostrando a lista de tarefas
+    for tarefa in lista_tarefas:
+        print("Tarefas:")
+        print(f"{lista_tarefas.index(tarefa) + 1} - {tarefa}") # mostrando cada tarefa
 elif escolha == 2:
     nova_tarefa = input("Qual tarefa deseja colocar na lista?") # perguntando qual tarefa deseja incluir
     lista_tarefas.append(nova_tarefa) # adicionando tarefa a lista de tarefas
